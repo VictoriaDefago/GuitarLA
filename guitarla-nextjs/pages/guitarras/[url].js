891c4354ec3f33b19366be7ef34960eebf86380b
VictoriaDefagot <defagotvictoria@gmail.com>
2023-06-08 22:44:56 -0300

    Dynamic routing with getServerSideProps()

---
+++ guitarla-nextjs/pages/guitarras/[url].js
@@ -0,0 +1,24 @@
+export default function Product({guitar}) {
+
+    console.log(guitar[0].attributes.name)
+
+  return (
+    <div>
+      url
+    </div>
+  )
+
+}
+
+
+export async function getServerSideProps({query: { url }}) {
+
+    const response = await fetch(`${process.env.API_URL}/guitars?filters[url]=${url}&populate=image`)
+    const { data: guitar } = await response.json()
+
+    return {
+        props: {
+            guitar
+        }
+    }
+}
