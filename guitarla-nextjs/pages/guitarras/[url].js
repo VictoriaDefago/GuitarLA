aaf8f85d6e535199b2d929c74199dd9d3f10757b
VictoriaDefagot <defagotvictoria@gmail.com>
2023-06-08 22:57:43 -0300

    Dynamic routing with getStaticProps() and getStaticPaths()

--- guitarla-nextjs/pages/guitarras/[url].js
+++ guitarla-nextjs/pages/guitarras/[url].js
@@ -11,7 +11,39 @@ export default function Product({guitar}) {
 }
 
 
-export async function getServerSideProps({query: { url }}) {
+// export async function getServerSideProps({query: { url }}) {
+
+//     const response = await fetch(`${process.env.API_URL}/guitars?filters[url]=${url}&populate=image`)
+//     const { data: guitar } = await response.json()
+
+//     return {
+//         props: {
+//             guitar
+//         }
+//     }
+// }
+
+
+
+
+export async function getStaticPaths() {
+
+    const response = await fetch(`${process.env.API_URL}/guitars`)
+    const { data } = await response.json()
+
+    const paths = data.map( guitar => ({
+        params: {
+            url: guitar.attributes.url
+        }
+    }))
+
+    return {
+        paths,
+        fallback: false
+    }
+}
+
+export async function getStaticProps({params: { url }}) {
 
     const response = await fetch(`${process.env.API_URL}/guitars?filters[url]=${url}&populate=image`)
     const { data: guitar } = await response.json()
