57a05f14a17fca9f0d0e55258007827bc8e87573
VictoriaDefagot <defagotvictoria@gmail.com>
2023-06-19 12:06:48 -0300

    Shopping cart add product functionality

--- guitarla-nextjs/pages/_app.js
+++ guitarla-nextjs/pages/_app.js
@@ -1,5 +1,52 @@
+import { useState } from 'react'
 import '@/styles/globals.css'
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+
+  const [shoppingCart, setShoppingCart] = useState([])
+
+  const addShoppingCart = guitar => {
+    if(shoppingCart.some( guitarState =>  guitarState.id === guitar.id )) {
+
+      const updatedShoppingCart = shoppingCart.map( guitarState => {
+        if( guitarState.id === guitar.id ) {
+          guitarState.quantity = guitar.quantity;
+        } 
+        return guitarState;
+      });
+
+      setShoppingCart([...updatedShoppingCart]);
+      localStorage.setItem('carrito', JSON.stringify( shoppingCart ));
+      
+    } else {
+      setShoppingCart([...shoppingCart, guitar]);
+      localStorage.setItem('carrito', JSON.stringify( shoppingCart ));
+    }
+  }
+
+  const deleteProduct = id => {
+    const updatedShoppingCart = shoppingCart.filter( product => product.id != id)
+    setShoppingCart(updatedShoppingCart)
+    window.localStorage.setItem('carrito', JSON.stringify( shoppingCart ));
+  }
+
+  const updateQuantity = guitar => {
+    const updatedShoppingCart = shoppingCart.map( guitarState => {
+      if(guitarState.id === guitar.id ) {
+        guitarState.cantidad = parseInt( guitar.quantity )
+      } 
+      return guitarState
+    })
+    setShoppingCart(updatedShoppingCart)
+    window.localStorage.setItem('carrito', JSON.stringify( shoppingCart ));
+  }
+
+
+  return <Component {...pageProps} 
+    shoppingCart={shoppingCart}
+    addShoppingCart={addShoppingCart}
+    deleteProduct={deleteProduct}
+    updateQuantity={updateQuantity}
+  />
+
 }
--- guitarla-nextjs/pages/guitarras/[url].js
+++ guitarla-nextjs/pages/guitarras/[url].js
@@ -1,23 +1,58 @@
+import { useState } from 'react'
 import Image from "next/image"
 import Layout from '../../components/layout'
 import styles from '../../styles/guitars.module.css'
 
-export default function Product({guitar}) {
+export default function Product({guitar, addShoppingCart}) {
 
+    const [quantity, setQuantity] = useState(0)
     const { name, description, price, image } = guitar[0].attributes
 
-  return (
-    <Layout title={`Guitarra ${name}`}>
-        <div className={styles.guitar}>
-            <Image src={image.data.attributes.url} width={600} height={400} alt={`Image Guitar ${name}`} />
-            <div className={styles.content}>
-                <h3>{name}</h3>
-                <p className={styles.description}>{description}</p>
-                <p className={styles.price}>${price}</p>
+    const handleSubmit = e => {
+        e.preventDefault()
+
+        if(quantity<1) {
+            alert('Cantidad no válida')
+            return
+        }
+
+        const selectedGuitar = {
+            id: guitar[0].id,
+            image: image.data.attributes.url,
+            name,
+            price,
+            quantity
+        }
+
+        addShoppingCart(selectedGuitar)
+
+    }
+
+    return (
+        <Layout title={`Guitarra ${name}`}>
+            <div className={styles.guitar}>
+                <Image src={image.data.attributes.url} width={600} height={400} alt={`Image Guitar ${name}`} />
+                <div className={styles.content}>
+                    <h3>{name}</h3>
+                    <p className={styles.description}>{description}</p>
+                    <p className={styles.price}>${price}</p>
+
+                    <form className={styles.form} onSubmit={handleSubmit} >
+                        <label htmlFor="quantity">Cantidad:</label>
+                        <select id="quantity" onChange={ e => setQuantity(+e.target.value) }>
+                            <option value="0">--Seleccione--</option>
+                            <option value="1">1</option>
+                            <option value="2">2</option>
+                            <option value="3">3</option>
+                            <option value="4">4</option>
+                            <option value="5">5</option>
+                        </select>
+                        <input type="submit" value="Agregar al carrito" />
+                    </form>
+                </div>
             </div>
-        </div>
-    </Layout>
-  )
+        </Layout>
+    )
 
 }
 
